35bab7a05946ae6d70a23a2022c1f8db5dcf1e7d
njsingh15 <33374027+njsingh15@users.noreply.github.com>
2018-01-08 19:32:50 +0530

    Update LoginPage.java

--- src/main/java/com/kattapa/login/LoginPage.java
+++ src/main/java/com/kattapa/login/LoginPage.java
@@ -4,7 +4,7 @@ public class LoginPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Login page is excecuted..");
+		System.out.println("Login page is excecuted....");
 
 	}
 
